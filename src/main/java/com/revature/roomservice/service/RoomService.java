5235a3d7eaf297e5ac0c5d0de5d2c75430171a65
Steven Rong <88406912+CrossedCoding@users.noreply.github.com>
2022-04-18 13:48:31 -0700

    Updated integration tests

--- src/main/java/com/revature/roomservice/service/RoomService.java
+++ src/main/java/com/revature/roomservice/service/RoomService.java
@@ -3,8 +3,6 @@ package com.revature.roomservice.service;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -16,63 +14,81 @@ import com.revature.roomservice.repo.RoomRepo;
 public class RoomService {
 
 	@Autowired
-    private RoomRepo roomRepo;
-	
+	private RoomRepo roomRepo;
+
 	@Autowired
 	private RestTemplate restTemplate;
-	
-    @Autowired
-    public RoomService(RoomRepo roomRepo){
-        super();
-        this.roomRepo = roomRepo;
-    }
 
-    public List<Room> findAll(){
-        return roomRepo.findAll();
-    }
+	@Autowired
+	public RoomService(RoomRepo roomRepo) {
+		super();
+		this.roomRepo = roomRepo;
+	}
 
-    public Room findById(int id){
-        try{
-            return roomRepo.findById(id).get();
-        } catch (NoSuchElementException e){
-            e.printStackTrace();
-            return null;
-        }
-       
-    }
-    
-    public Area findbyName(String name) {
-    	return restTemplate.getForObject("http://localhost:8093/area/name/"+name, Area.class);
-    }
-    
-    public List<Room> findByAreaName(Area area) {
-    	
-    	return roomRepo.findByArea(area);
-    
-    }
-    
-    public List<Room> findByDoctor(User doctor) {
-    	
-    	return roomRepo.findByDoctor(doctor);
-    	
-    	// return restTemplate.getForObject("" + doctor, User.class);
-    	
-    }
-    
-   public List<Room> findByNurse(User nurse) {
-    	
-    	return roomRepo.findByDoctor(nurse);
-    	
-    	// return restTemplate.getForObject("" + doctor, User.class);
-    	
-    }
-    
-    public Room findByPatient(Patient patient) {
-    
-    	return roomRepo.findByPatient(patient);
-    
-	}	
-    
-    // Add notify when transfer
-    
+	public List<Room> findAll() {
+		return roomRepo.findAll();
+	}
+
+	public Room findById(int id) {
+		try {
+			return roomRepo.findById(id).get();
+		} catch (NoSuchElementException e) {
+			e.printStackTrace();
+			return null;
+		}
+
+	}
+
+	public Area findbyName(String name) {
+
+		try {
+
+			return restTemplate.getForObject("http://localhost:8093/area/name/" + name, Area.class);
+		} catch (NoSuchElementException e) {
+
+			e.printStackTrace();
+			return null;
+
+		}
+	}
+
+	public List<Room> findByAreaName(Area area) {
+
+		try {
+
+			return roomRepo.findByArea(area);
+
+		} catch (NoSuchElementException e) {
+			
+			e.printStackTrace();
+			return null;
+			
+		}
+		
+	}
+
+	public List<Room> findByDoctor(User doctor) {
+
+		return roomRepo.findByDoctor(doctor);
+
+		// return restTemplate.getForObject("" + doctor, User.class);
+
+	}
+
+	public List<Room> findByNurse(User nurse) {
+
+		return roomRepo.findByDoctor(nurse);
+
+		// return restTemplate.getForObject("" + doctor, User.class);
+
+	}
+
+	public Room findByPatient(Patient patient) {
+
+		return roomRepo.findByPatient(patient);
+
+	}
+
+	// Add notify when transfer
+
 }
